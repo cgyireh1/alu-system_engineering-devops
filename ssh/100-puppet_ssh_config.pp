29606a13dc017e0482049d54ce4e441ee6cca6fe
cgyireh <c.gyireh@alustudent.com>
2023-07-22 16:52:51 -0700

    advanced

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# Changes configuration file to turn off password authent.
+# Use private key ~/.ssh/school
+Host 107.21.139.186
+  PasswordAuthentication no
+  
+Host 107.21.139.186
+  IdentityFile ~/.ssh/school
